d21101c6c94525ebe27bfc7214c50853d330553f
Max Larsson <max.larsson@liu.se>
2018-05-22 16:24:06 +0200

    Bumped version.

--- plotprofile/version.py
+++ plotprofile/version.py
@@ -1,8 +1,8 @@
 import os.path
 import sys
 
-version = "0.1.0"
-date = ("May", "18", "2018")
+version = "0.2.0"
+date = ("May", "22", "2018")
 title = "PlotProfile"
 author = "Max Larsson"
 email = "max.larsson@liu.se"
